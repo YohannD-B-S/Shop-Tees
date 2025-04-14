f71fd090706932c3810be86c9fc74af0f6245bc6
Yohann Dubuis <yohann.dubuis@lapiscine.pro>
2025-04-14 12:07:26 +0200

    creation d'une class

---
+++ model/order.model.php
@@ -0,0 +1,12 @@
+<?php
+
+class Order{ //creation d'une class Order qui viendra remplacer le tableau des articles 
+
+    public $product; // dans cette classe : creation d'une variable product dans laquelle on pourra stocker le nom ou l'id d'un produit.
+
+    public $quantity; // creation d'une variable quantité dans laquelle nous pourrons stcker une quantité de produit commandé
+
+    public $status; // creation d'un varliable status dans laquelle on pourra stocker le status de la commande 
+
+    public $createdAt; // creation d'une variable createdAt afin d'y stoquer la date du produit.
+}
---
+++ view/ship-order.view.php
@@ -0,0 +1,17 @@
+<?php require_once ('../view/partial/header.php'); ?>
+
+<main>
+	
+	<h1>Expédition de commande</h1>
+
+
+	<p><?php echo $message; ?></p>
+
+	<form method="post">
+		<button type="submit">Expédiez cette commande</button>
+	</form>
+
+</main>
+
+</body>
+</html>
